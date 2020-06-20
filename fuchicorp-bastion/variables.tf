90d4314f3d328333f7f5da2275762035b25bd3ee
Volodymyr Khomenko <volodymyr1213@bastion-fuchicorp.us-central1-a.c.solid-antler-278903.internal>
2020-06-20 21:51:16 +0000

    Fixed branch

--- fuchicorp-bastion/variables.tf
+++ fuchicorp-bastion/variables.tf
@@ -3,9 +3,27 @@ variable "instance_name" {
   description = "- (Optional) Give an any name on bastion host"
 }
 
+variable "vpc_network" {
+  default = "google_compute_network"
+  description = "- (Optional) An optional description of this resource. The resource must be recreated to modify this field"
+}
+
+variable "google_compute_firewall" {
+  default = "test-firewall"
+  description = "- (Optional) An optional description of this resource. The resource must be recreated to modify this field"
+  # description = "name - (Required) Name of the resource. Provided by the client when the resource is created.
+}
+
+
+
 variable "google_project_id" {
   default = "fuchicorp-project-256020"
   description = "- (Optional) That is gonna be used for our particular project ID in GCP"  
+}
+
+variable "google_domain_name" {
+  default = "fuchicorp.com"
+  description = "- (Optional) The Domain Name"  
 }
 
 variable "zone" {
